06dce390d0a6a7530f43d80b66088d66ec10335e
Ana-gram <73074439+Ana-gram@users.noreply.github.com>
2021-03-06 21:37:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,4 +53,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("NzYwMTcxODEzODY2NzAwODUw.X3ILKw.U0khIDApE1G1Z4lVmLQ8a6Wd0GY")
+client.run("X")
